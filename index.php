3728e051226eb4b5a77b5a073166ac73ff4882d5
2020ASP16Thilakshika <139615320+2020ASP16Thilakshika@users.noreply.github.com>
2024-12-09 16:41:18 +0000

    edit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Beauty Bliss Website</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+	<header>
+		<a href="index.php" class="logo">Beauty<span>Bliss</span></a>
+		<nav class="navbar">
+			<a href="index.php">Home</a>
+			<a href="products.php">Products</a>
+			<a href="purchases.php">Purchases</a>
+			<a href="account.php">Account</a>
+		</nav>
+	</header>
+	<section class="home">
+		<div class="content">
+			<h3>Skin Care & Cosmetics</h3>
+			<span>Enhance Your Beauty</span>
+			<a href="products.php" class="btn">Order Now</a>
+		</div>
+	</section>
+	<footer>
+		<p>&copy; 2024 Beauty Bliss. All Rights Reserved.</p>
+	</footer>
+</body>
+</html>
