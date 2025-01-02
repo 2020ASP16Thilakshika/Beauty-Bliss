18f5acfff998b7af04a30d852d8b299b46d224ff
2020ASP16Thilakshika <139615320+2020ASP16Thilakshika@users.noreply.github.com>
2025-01-02 23:16:47 +0000

    edit

--- gettable.php
+++ gettable.php
@@ -1,24 +1,45 @@
+<?php
+//set the db connection file
+require_once 'db.php';
 
+try{
+	//Query
+	$sql = "SELECT * FROM users";
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>STUDENT LIST</title>
-</head>
-<body>
+	//execute the query
+	$result = mysqli_query($connect,$sql);
 
-	<?php
-//get the db connection file
+	//check if data exists in the table
+	if(mysqli_num_rows($result)>0){
+		//fetch the data from rows
+		echo "<table border=1>";
+		$col = mysqli_fetch_fields($result);
+		//print the columns
+		echo "<tr>";
+		foreach($col as $value){
+			//return object
+			//print_r($value)
+			echo "<td>$value->name</td>";
+		}
+		echo "</td>";
 
-require_once 'dbconf.php'; //(conf/dbconf.php) (folder/file)
-require_once 'myfun.php';
-
-//PrintTable ("student",$connect);
-student($connect);
-
+		while($row = mysqli_fetch_assoc($result)){
+			//print the data in a table form
+			echo "<tr>";
+			foreach($row as $key => $value){
+				echo "<td>$value</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table>";
+	}
+	else{
+		echo "No result";
+	}
+}
+catch(Exception $e){
+	die($e->getMessage());
+}
 ?>
 
 
-
-</body>
-</html>
