6e495419e1d178d3c6f21149c6791c5b406c38ad
2020ASP16Thilakshika <139615320+2020ASP16Thilakshika@users.noreply.github.com>
2025-01-03 00:23:35 +0000

    edit

--- func.php
+++ func.php
@@ -1,51 +1,151 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
 <?php
-
-	require_once 'db.php';	
-	
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-			//echo "Got the POST request from client";
-			   // $id = $_POST['id'];
-				$full_name = $_POST['full_ name'];
-				$email = $_POST['email'];
-				$password = $_POST['password'];
-			
-				AddData($conn,$full_name,$email,$password);
-	}
-			
-function AddData($connect,$full_name,$email,$password){
-			try{
-
-                //Quary
-                $sql = "INSERT INTO users VALUES('$full_name','$email','$password')";
-                //execute the quary
-                $result = mysqli_query($connect,$sql);
-
-                if ($result){
-                    echo "New user record created successfully";
-                    printTable("users",$connect);
-                }else{
-                    die("Error".mysqli_error($connect));
-                }
-
-		    } catch(Exception $e){
-			    die($e->getMessage());
-	        }
-		}
-    
+//get the database connection file
+require_once 'dbconf.php';
 function PrintTable($tableName,$connect){
+try{
+	//Query
+	$sql = "SELECT * FROM $tableName";
+
+	//execute the query (call variable,query)
+	$result = mysqli_query($connect,$sql);
+
+	if (mysqli_num_rows($result)>0) {
+		//fetch the data from rows
+
+		echo "<table border=1> ";
+
+	$col = mysqli_fetch_fields($result);
+	//print the column
+	echo "<tr>";
+	foreach ($col as $value) {
+		//return an object
+		//print_r($value)
+			echo "<td>".$value->name."</td>";
+		}
+		echo "</tr>";
+		while($row = mysqli_fetch_assoc($result)){
+			//print the data in table format
+
+		echo "<tr>";
+		foreach ($row as $key => $value) {
+			echo "<td>$value</td>";
+		}
+		echo "</tr>";
+		}
+		echo "</table>";
+	} 
+	else{
+		echo "No results"; //table is empty
+	}
+
+	}
+
+
+catch(Exception $e){
+	die($e->getMessage());
+}
+}
+ //GET DATA FROM DB
+ function users($connect){
             try{
             
-                $sql = "SELECT * FROM $tableName";
+                $sql = "SELECT id,full_name FROM  users";
+            
+            
+                $result = mysqli_query($connect,$sql);
+            
+                if (mysqli_num_rows($result)>0) {
+                
+            
+                    echo "<table border=1> ";
+            
+                $col = mysqli_fetch_fields($result);
+            
+                echo "<tr>";
+                foreach ($col as $value) {
+                    
+                        echo "<td>".$value->name."</td>";
+                    }
+
+                    echo "<td> View details </td>";
+                    echo "</tr>";
+                    while($row = mysqli_fetch_assoc($result)){ 
+                    echo "<tr>";
+                    foreach ($row as $key => $value) {
+                        echo "<td>$value</td>";
+                    }
+                    $id=$row['id'];
+                    //query string
+                    echo "<td><a href='printtable.php? id=$id '> View </a> </td>";
+                    echo "</tr>";
+                    
+                    }
+
+                    echo "</table>";
+                } 
+                else{
+                    echo "No results"; 
+                }
+            
+                }
+            
+            
+            catch(Exception $e){
+                die($e->getMessage());
+            }
+        }
+
+
+
+         function Usersdetails($id,$connect){
+            try{
+            
+                $sql = "SELECT * FROM  users where  id = id ";
+            
+            
+                $result = mysqli_query($connect,$sql);
+            
+                if (mysqli_num_rows($result)>0) {
+                
+            
+                    echo "<table border=1> ";
+            
+                $col = mysqli_fetch_fields($result);
+            
+                echo "<tr>";
+                foreach ($col as $value) {
+                    
+                        echo "<td>".$value->name."</td>";
+                    }
+                    echo "</tr>";
+                    while($row = mysqli_fetch_assoc($result)){
+                        
+                    echo "<tr>";
+                    foreach ($row as $key => $value) {
+                        echo "<td>$value</td>";
+                    }
+                    echo "</tr>";
+                    }
+                    echo "</table>";
+                } 
+                else{
+                    echo "No results"; 
+                }
+            
+                }
+            
+            
+            catch(Exception $e){
+                die($e->getMessage());
+            }
+        }
+
+       
+	
+       function Jointable($id,$connect){
+            try{
+            
+                $sql = "SELECT * FROM users INNER JOIN purchases ON users.id = purchases.user_id;
             
             
                 $result = mysqli_query($connect,$sql);
@@ -85,6 +185,3 @@ function PrintTable($tableName,$connect){
             }
         }
 ?>
-<a href="account.php">Go back</a>
-</body>
-</html>
