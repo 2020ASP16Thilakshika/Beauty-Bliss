b22fbbc2707ad918f53c97609f24cbe667c1ff56
2020ASP16Thilakshika <139615320+2020ASP16Thilakshika@users.noreply.github.com>
2025-01-03 00:25:55 +0000

    edit

--- func.php
+++ func.php
@@ -153,7 +153,7 @@ catch(Exception $e){
                 if (mysqli_num_rows($result)>0) {
                 
             
-                    echo "<table border=1> ";
+                   // echo "<table border=1>";
             
                 $col = mysqli_fetch_fields($result);
             
